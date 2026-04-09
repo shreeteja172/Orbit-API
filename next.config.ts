74fc2ead2b6e55b75fc6f868720312a2687c8e2a
shreeteja22 <smutukundu2206@gmail.com>
2026-04-09 08:21:51 +0530

    chore: update next.config.ts for deploying in aws amplify

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone', // Optional but recommended for smaller builds
+  images: {
+    unoptimized: true, // Use if you're having issues with image optimization
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
